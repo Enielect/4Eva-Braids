589d0267fb307b438084ac66774ca6f62ebb895f
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2025-01-12 11:07:29 +0100

    Replaced place holders in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,33 +18,28 @@ const AboutPage = () => {
           />
         </div>
         <div className='-order-2 md:max-w-[40rem] md:flex justify-center flex-col items-center text-center'>
-          <Heading className='lg:text-5xl'>about the company</Heading>
+          <Heading className='lg:text-5xl'>About the company</Heading>
           <p className=''>
-            Lorem ipsum dolor sit amet consectetur. Ipsum urna eget quis
-            tristique posuere leo tincidunt mi nisl. Leo mattis vel phasellus
-            praesent leo pharetra lobortis. Non facilisi venenatis vitae
-            ultricies sociis volutpat suspendisse dictum sodales. Interdum massa
-            pulvinar cursus dignissim nisl. Elit purus egestas cursus semper eu
-            commodo. Bibendum pharetra aliquet vitae faucibus aliquam mattis in
-            pretium. Et molestie a commodo in leo amet. Tortor ac faucibus sit
-            vitae vitae commodo id. Eget vel eget mattis id nec odio fermentum
-            ac egestas. Posuere sed aliquam leo dui justo sapien. Sed eget
-            scelerisque ultricies elit. Tristique metus gravida nunc massa sit.
-            Augue quis ut sit nulla. Cras id netus euismod tempus urna.
+          <strong><em>4evabraids</em> is a next generation braiding salon</strong> that offers a wide range of braiding services. The company was founded in 2019
+          and since then we have retained the reputation a excellence in the industry. <br />
+          <br />
+
+          At <em>4evabraids</em> we’re revolutionizing the hair braiding industry with our next-generation salon experience.
+          Whether you prefer the comfort of your home or the vibrant energy of our in-salon service, our team of highly skilled,
+          professional braiders is committed to crafting stunning, long-lasting styles tailored to you.
+          Renowned for our exceptional craftsmanship, <em>4evabraids</em> has become synonymous with premium quality and innovative 
+          techniques that set us apart from the rest. With an expanding presence across Lagos and plans to launch nationwide soon,
+          we’re bringing our artistry and excellence to more clients who demand nothing but the best.
           </p>
         </div>
       </section>
       <section className="bg-[url('/circular-bg.jpeg')] md:py-12 py-5 space-y-6">
         <div className='md:flex md:max-w-[70rem] mx-auto'>
           <IntentCard icon={<EyeIcon />} title='our vision'>
-            Lorem ipsum dolor sit amet consectetur. Iaculis metus sodales ut
-            lacus. Cursus pharetra ornare lacus pretium purus aliquam leo. Quis
-            dui.
+            To be the leading all-in-one hair braiding salon in the world, bringing beauty, confidence, and innovation to every client.
           </IntentCard>
           <IntentCard icon={<MissionIcon />} title='our mission'>
-            Lorem ipsum dolor sit amet consectetur. Iaculis metus sodales ut
-            lacus. Cursus pharetra ornare lacus pretium purus aliquam leo. Quis
-            dui.
+            At <em>4evabraids</em>, we aim to transform the hair braiding experience by combining artistry, innovation, and unmatched professionalism
           </IntentCard>
         </div>
       </section>
@@ -53,24 +48,24 @@ const AboutPage = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-12'>
           {[
             {
-              value: 'value',
+              value: 'Elegance',
               description:
-                'Lorem ipsum dolor sit amet consectetur. Viverra massa odio cras lorem habitant tempor. Ornare mattis orci praesent rhoncus facilisis convallis a facilisis nullam. Sit pharetra arcu ultrices tristique.',
+                'Our dedication to elegance ensures every braid is crafted with sophistication, enhancing your style with a flawless finish.',
             },
             {
-              value: 'value',
+              value: 'Efficiency',
               description:
-                'Lorem ipsum dolor sit amet consectetur. Viverra massa odio cras lorem habitant tempor. Ornare mattis orci praesent rhoncus facilisis convallis a facilisis nullam. Sit pharetra arcu ultrices tristique.',
+                'We value your time by delivering exceptional braids promptly, maintaining the perfect balance of speed and quality.',
             },
             {
-              value: 'value',
+              value: 'Quality',
               description:
-                'Lorem ipsum dolor sit amet consectetur. Viverra massa odio cras lorem habitant tempor. Ornare mattis orci praesent rhoncus facilisis convallis a facilisis nullam. Sit pharetra arcu ultrices tristique.',
+                'Experience unmatched quality in every braid, as we use the finest techniques and materials to guarantee durability and style.',
             },
             {
-              value: 'value',
+              value: 'Perfection',
               description:
-                'Lorem ipsum dolor sit amet consectetur. Viverra massa odio cras lorem habitant tempor. Ornare mattis orci praesent rhoncus facilisis convallis a facilisis nullam. Sit pharetra arcu ultrices tristique.',
+                'Perfection is our standard. Each braid is a masterpiece, tailored to elevate your look and boost your confidence.',
             },
           ].map((ele, i) => (
             <CoreValuesCard key={i} index={i + 1} title={ele.value}>
@@ -92,39 +87,21 @@ const AboutPage = () => {
         <div className=''>
           <Heading className='lg:text-5xl lg:text-left'>meet the ceo</Heading>
           <p>
-            Lorem ipsum dolor sit amet consectetur. Ipsum urna eget quis
-            tristique posuere leo tincidunt mi nisl. Leo mattis vel phasellus
-            praesent leo pharetra lobortis. Non facilisi venenatis vitae
-            ultricies sociis volutpat suspendisse dictum sodales. Interdum massa
-            pulvinar cursus dignissim nisl. Elit purus egestas cursus semper eu
-            commodo. Bibendum pharetra aliquet vitae faucibus aliquam mattis in
-            pretium. Et molestie a commodo in leo amet. Tortor ac faucibus sit
-            vitae vitae commodo id. Eget vel eget mattis id nec odio fermentum
-            ac egestas. Posuere sed aliquam leo dui justo sapien. Sed eget
-            scelerisque ultricies elit. Tristique metus gravida nunc massa sit.
-            Augue quis ut sit nulla. Cras id netus euismod tempus urna. Lorem
-            ipsum dolor sit amet consectetur. Ipsum urna eget quis tristique
-            posuere leo tincidunt mi nisl. Leo mattis vel phasellus praesent leo
-            pharetra lobortis. Non facilisi venenatis vitae ultricies sociis
-            volutpat suspendisse dictum sodales. Interdum massa pulvinar cursus
-            dignissim nisl. Elit purus egestas cursus semper eu commodo.
-            Bibendum pharetra aliquet vitae faucibus aliquam mattis in pretium.
-            Et molestie a commodo in leo amet. Tortor ac faucibus sit vitae
-            vitae commodo id. Eget vel eget mattis id nec odio fermentum ac
-            egestas. Posuere sed aliquam leo dui justo sapien. Sed eget
-            scelerisque ultricies elit. Tristique metus gravida nunc massa sit.
-            Augue quis ut sit nulla. Cras id netus euismod tempus urna. Lorem
-            ipsum dolor sit amet consectetur. Ipsum urna eget quis tristique
-            posuere leo tincidunt mi nisl. Leo mattis vel phasellus praesent leo
-            pharetra lobortis. Non facilisi venenatis vitae ultricies sociis
-            volutpat suspendisse dictum sodales. Interdum massa pulvinar cursus
-            dignissim nisl. Elit purus egestas cursus semper eu commodo.
-            Bibendum pharetra aliquet vitae faucibus aliquam mattis in pretium.
-            Et molestie a commodo in leo amet. Tortor ac faucibus sit vitae
-            vitae commodo id. Eget vel eget mattis id nec odio fermentum ac
-            egestas. Posuere sed aliquam leo dui justo sapien. Sed eget
-            scelerisque ultricies elit. Tristique metus gravida nunc massa sit.
-            Augue quis ut sit nulla. Cras id netus euismod tempus urna.
+            <strong>Nwofia Chidinma Evaresta</strong> is the visionary CEO and Chief Braider of <em>4evabraids</em>, a brand synonymous with
+          innovation and excellence in the hair braiding industry. With a unique ability to craft masterpieces
+          from every client’s hair, Evaresta has transformed her passion for braiding into a thriving business
+          that redefines beauty and artistry. <br />
+          <br />
+          
+          A graduate of English from Nnamdi Azikiwe University (UNIZIK), 
+          Evaresta’s journey into braiding began long before her professional career. From her primary school days,
+          she displayed remarkable skill and creativity, braiding her classmates' hair with precision and flair.
+          After graduating, she turned her lifelong talent into a career, building a reputation as a diligent, innovative, and highly skilled braider.
+          Today, Evaresta doesn’t just braid hair—she creates styles for greatness. Her work graces the heads of 
+          influential individuals, reflecting her commitment to delivering beauty, confidence, and unmatched quality.
+
+          Under her leadership, <em>4evabraids</em> continues to grow, expanding its reach and reputation while staying true to the
+          values of excellence, creativity, and passion that define Evaresta herself.
           </p>
         </div>
       </section>
