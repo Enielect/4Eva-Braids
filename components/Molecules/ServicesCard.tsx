20d761436ee60640bae65dfdbe06d0b2a2eb221f
Salq24 <142992679+Salq24@users.noreply.github.com>
2025-01-13 21:14:40 +0000

    replaced placeholder images

--- components/Molecules/ServicesCard.tsx
+++ components/Molecules/ServicesCard.tsx
@@ -13,7 +13,7 @@ const ServicesCard = (props: {
       <div className='max-sm:max-w-[200px] rounded-xl max-h-[150px] sm:max-w-[230px] border  overflow-hidden mx-auto'>
         <Image
           className='w-full object-cover'
-          src={props.image}
+          src={Model1}
           width={400}
           height={400}
           alt='services'
